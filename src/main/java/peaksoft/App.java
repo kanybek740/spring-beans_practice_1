ac935d170218233e0f80588899568099f8b47c2d
kanybek_draft2 <kanybek1992@gmail.com>
2021-10-18 01:57:58 +0300

    spring_homework_1

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -4,16 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import peaksoft.config.AppConfig;
 import peaksoft.model.AnimalsCage;
+import peaksoft.model.Timer;
 
 public class App {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
+            AnimalsCage bean = context.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
+//        Timer timer = context.getBean("timer", Timer.class);
+//        timer.getTime();
     }
 }
--- src/main/java/peaksoft/model/Timer.java
+++ src/main/java/peaksoft/model/Timer.java
@@ -1,6 +1,11 @@
 package peaksoft.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
